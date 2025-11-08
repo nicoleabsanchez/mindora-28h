4bb039c1e14ef5c8a762c2d96e9f12fb9f85b60b
Nicole Sanchez <nicoleabsanchez@gmail.com>
2025-11-08 16:45:34 -0500

    Add 100% responsive design with comprehensive media queries for all devices

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./styles/custom-improvements.css";
+import "./styles/responsive.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
   
